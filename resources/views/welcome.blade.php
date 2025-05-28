54e8842711535c76df1298895c4860c7816d899c
Timothy <oludefiyinfoluwa06@gmail.com>
2025-05-28 16:27:15 +0100

    Initial setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClinicConnect</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="text-center px-8 py-12">
+    <h1>Welcome to ClinicConnect</h1>
+</body>
+</html>
